f94d46eac38caf05302450977199c7932a174617
John Forecast <john@forecast.name>
2023-12-28 13:02:57 -0500

    Latest kernel changes to version 3.0.6

--- kernel/dnet.h
+++ kernel/dnet.h
@@ -33,7 +33,7 @@
  * Define the kernel revision. Note that we skipped 2.0.0 so the userland and
  * kernel can be aligned.
  */
-#define DNET_REVISION   "3.0.5"
+#define DNET_REVISION   "3.0.6"
 
 struct dn_scp;
 
